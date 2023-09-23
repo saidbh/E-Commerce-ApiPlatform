877de614784ccc939ab17fc850e97bd00085ec8e
saidbenhmed3 <saidbenhmed3@gmail.com>
2023-09-23 15:55:20 +0100

    init

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    #[Route('/product', name: 'app_product')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to Siwar application',
+            'path' => 'src/Controller/ProductController.php',
+        ]);
+    }
+}
